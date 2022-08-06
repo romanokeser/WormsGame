457c3ec9aa005d6cf1134bc38bacd4e3c459a9bb
RomanoK <romanokeser@gmail.com>
2022-08-06 18:04:23 +0200

    Destroying ground on bullet hit

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -4,137 +4,135 @@ using UnityEngine;
 
 public class Ground : MonoBehaviour
 {
-    [SerializeField] private Texture2D _baseTexture;
+    public Texture2D baseTexture;
+    Texture2D cloneTexture;
+    SpriteRenderer sr;
 
-    private Texture2D _cloneTexture;
-    private SpriteRenderer _spriteRenderer;
-
-    private float _widthWorld, _heightWorld;
-    private float _widthPixel, _heightPixel;
-
-    void Start()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-
-        _cloneTexture = Instantiate(_baseTexture);
-
-        _cloneTexture.alphaIsTransparency = true;
-
-        UpdateTexture();
-
-        gameObject.AddComponent<PolygonCollider2D>();
-    }
-
-    #region Getters
+    float widthWorld, heightWorld;
+    int widthPixel, heightPixel;
 
     public float WidthWorld
     {
         get
         {
-            if (_widthWorld == 0)
-                _widthWorld = _spriteRenderer.bounds.size.x;
-
-            return _widthWorld;
+            if (widthWorld == 0)
+                widthWorld = sr.bounds.size.x;
+            return widthWorld;
         }
-    }
 
+    }
     public float HeightWorld
     {
         get
         {
-            if (_heightWorld == 0)
-                _heightWorld = _spriteRenderer.bounds.size.y;
-
-            return _heightWorld;
+            if (heightWorld == 0)
+                heightWorld = sr.bounds.size.y;
+            return heightWorld;
         }
+
     }
-
-
-    public float WidthPixel
+    public int WidthPixel
     {
         get
         {
-            if (_widthPixel == 0)
-                _widthPixel = _spriteRenderer.sprite.texture.height;
+            if (widthPixel == 0)
+                widthPixel = sr.sprite.texture.width;
 
-            return _widthPixel;
+            return widthPixel;
         }
     }
-
-    public float HeightPixel
+    public int HeightPixel
     {
         get
         {
-            if (_heightPixel == 0)
-                _heightPixel = _spriteRenderer.sprite.texture.height;
+            if (heightPixel == 0)
+                heightPixel = sr.sprite.texture.height;
 
-            return _heightPixel;
+            return heightPixel;
         }
     }
 
 
-    #endregion
-
-    private void UpdateTexture()
+    // Use this for initialization
+    void Start()
     {
-        _spriteRenderer.sprite = Sprite.Create(_cloneTexture,
-            new Rect(0, 0, _cloneTexture.width, _cloneTexture.height),
-            new Vector2(0.5f, 0.5f), 50f);
+        sr = GetComponent<SpriteRenderer>();
+        cloneTexture = Instantiate(baseTexture);
+        cloneTexture.alphaIsTransparency = true;
+
+        if (cloneTexture.format != TextureFormat.ARGB32)
+            Debug.LogWarning("Texture must be ARGB32");
+        if (cloneTexture.wrapMode != TextureWrapMode.Clamp)
+            Debug.LogWarning("wrapMode must be Clamp");
+
+        UpdateTexture();
+        gameObject.AddComponent<PolygonCollider2D>();
+
     }
 
-    Vector2Int World2Pixel(Vector2 pos)
+    void MakeAHole(CircleCollider2D col)
     {
-        Vector2Int v = Vector2Int.zero;
-        float dx = (pos.x - transform.position.x);
-        float dy = (pos.y - transform.position.y);
+        print(string.Format("{0},{1},{2},{3}", WidthPixel, HeightPixel, WidthWorld, heightWorld));
 
-        v.x = Mathf.RoundToInt(0.5f * WidthPixel + dx * (WidthPixel / WidthWorld));
-        v.y = Mathf.RoundToInt(0.5f * HeightPixel + dx * (HeightPixel / HeightWorld));
-
-        return v;
-    }
-
-    private void MakeAHole(CircleCollider2D col)
-    {
         Vector2Int c = World2Pixel(col.bounds.center);
-
         int r = Mathf.RoundToInt(col.bounds.size.x * WidthPixel / WidthWorld);
-        int px, nx, py, ny, d;
 
+        int px, nx, py, ny, d;
         for (int i = 0; i <= r; i++)
         {
-            d = Mathf.RoundToInt(Mathf.Sqrt(r * r - i * r));
-            for (int j = 0; j <= d ; j++)
+            d = Mathf.RoundToInt(Mathf.Sqrt(r * r - i * i));
+            for (int j = 0; j <= d; j++)
             {
                 px = c.x + i;
                 nx = c.x - i;
                 py = c.y + j;
                 ny = c.y - j;
 
-                _cloneTexture.SetPixel(px, py, Color.clear);
-                _cloneTexture.SetPixel(nx, py, Color.clear);
-                _cloneTexture.SetPixel(px, ny, Color.clear);
-                _cloneTexture.SetPixel(nx, ny, Color.clear);
+                cloneTexture.SetPixel(px, py, Color.clear);
+                cloneTexture.SetPixel(nx, py, Color.clear);
+                cloneTexture.SetPixel(px, ny, Color.clear);
+                cloneTexture.SetPixel(nx, ny, Color.clear);
             }
         }
-
-        _cloneTexture.Apply();
+        cloneTexture.Apply();
         UpdateTexture();
 
         Destroy(gameObject.GetComponent<PolygonCollider2D>());
-        gameObject.AddComponent<PolygonCollider2D>(); //setting collider as the same shape of the destroyed texture
+        gameObject.AddComponent<PolygonCollider2D>();
+    }
+
+    void UpdateTexture()
+    {
+        sr.sprite = Sprite.Create(cloneTexture,
+                            new Rect(0, 0, cloneTexture.width, cloneTexture.height),
+                            new Vector2(0.5f, 0.5f),
+                            50f
+                            );
+    }
+
+    Vector2Int World2Pixel(Vector2 pos)
+    {
+        Vector2Int v = Vector2Int.zero;
+
+        var dx = (pos.x - transform.position.x);
+        var dy = (pos.y - transform.position.y);
+
+        v.x = Mathf.RoundToInt(0.5f * WidthPixel + dx * (WidthPixel / WidthWorld));
+        v.y = Mathf.RoundToInt(0.5f * HeightPixel + dy * (HeightPixel / HeightWorld));
+
+        return v;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        //if (!collision.CompareTag("bullet"))
-        //    return;
 
-        //if (!collision.GetComponent<CircleCollider2D>())
-        //    return;
+        if (!collision.CompareTag("bullet"))
+            return;
+        if (!collision.GetComponent<CircleCollider2D>())
+            return;
 
-        //MakeAHole(collision.GetComponent<CircleCollider2D>());
-        //Destroy(collision.gameObject, 0.1f);
+        MakeAHole(collision.GetComponent<CircleCollider2D>());
+        Destroy(collision.gameObject, 0.1f);
     }
 
 }
